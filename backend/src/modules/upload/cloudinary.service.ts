e7f1e0e5af85989e7149db6d740f3805006fd25c
YourUserName <mfouda1810@gmail.com>
2026-03-24 04:51:50 +0200

    Update Cloudinary service to accept separate API keys

--- backend/src/modules/upload/cloudinary.service.ts
+++ backend/src/modules/upload/cloudinary.service.ts
@@ -9,6 +9,12 @@ export class CloudinaryService {
       cloudinary.config({
         url: process.env.CLOUDINARY_URL,
       });
+    } else if (process.env.CLOUDINARY_CLOUD_NAME) {
+      cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+      });
     }
   }
 
